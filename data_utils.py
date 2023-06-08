486bf5ec2115a2148af4d1355da3b1674ff207c9
mekaneeky <azawahry@sunbird.ai>
2023-06-09 01:48:54 +0300

    bugfixes

--- data_utils.py
+++ data_utils.py
@@ -48,7 +48,7 @@ class TextAudioLoader(torch.utils.data.Dataset):
 
         audiopaths_and_text_new = []
         lengths = []
-        for audiopath, text in self.audiopaths_and_text:
+        for audiopath, sid, text in self.audiopaths_and_text:
             if self.min_text_len <= len(text) and len(text) <= self.max_text_len:
                 audiopaths_and_text_new.append([audiopath, text])
                 lengths.append(os.path.getsize(audiopath) // (2 * self.hop_length))
